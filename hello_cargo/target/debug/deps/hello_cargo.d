1098d90f1e51821586297a5b2d57278dcf51ad2f
Bayego <edubayego@gmail.com>
2022-09-27 12:24:29 +0200

    hello, Cargo!

---
+++ hello_cargo/target/debug/deps/hello_cargo.d
@@ -0,0 +1,5 @@
+C:\Users\eduba\Desktop\Otros\Rust\Proyects\hello_cargo\target\debug\deps\hello_cargo.exe: src\main.rs
+
+C:\Users\eduba\Desktop\Otros\Rust\Proyects\hello_cargo\target\debug\deps\hello_cargo.d: src\main.rs
+
+src\main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+C:\Users\eduba\Desktop\Otros\Rust\Proyects\hello_cargo\target\debug\hello_cargo.exe: C:\Users\eduba\Desktop\Otros\Rust\Proyects\hello_cargo\src\main.rs
